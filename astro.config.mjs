989581bc795df0e15b29fedf9eb61f4de028c3a2
jboullion <jboullion83@gmail.com>
2024-05-11 14:20:41 -0500

    Netlify adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import vue from "@astrojs/vue";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [ vue({
-		appEntrypoint: "/src/_app.ts",
-		reactivityTransform: true
-	})]
+  integrations: [vue({
+    appEntrypoint: "/src/_app.ts",
+    reactivityTransform: true
+  })],
+  output: "server",
+  adapter: netlify()
 });
