05446553e5d3b9a27a17cd543ba827ff1fae923b
jboullion <jboullion83@gmail.com>
2024-05-10 22:37:15 -0500

    Basic layout and 3D model working!

--- src/components/vue/MyThreeViewer.vue
+++ src/components/vue/MyThreeViewer.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
-    <div id="productCanvas" class="" style="position: relative">
-      <i
+  <div class="d-flex flex-column h-100">
+    <div id="productCanvas" class="flex-fill" style="position: relative">
+      <div
         v-if="loading"
-        class="fas fa-cog fa-spin mb-0 h1 position-absolute top-50 start-50"
-      ></i>
+        class="spinner-border text-danger mb-0 position-absolute top-50 start-50"
+        role="status"
+      >
+        <span class="visually-hidden">Loading...</span>
+      </div>
     </div>
   </div>
 </template>
@@ -36,18 +39,9 @@ const animation = {
 const loading = ref(true);
 
 const debouncedResize = throttle(onWindowResize, 32);
-/**
- *
- *
- * SETUP
- *
- *
- */
 
 onMounted(async () => {
   await loadModels();
-
-  // updateCoords();
 
   setupScene();
 
@@ -61,7 +55,9 @@ onBeforeUnmount(() => {
 async function loadModels() {
   const loader = new GLTFLoader();
 
-  model = await loader.loadAsync("/models/lego_voltron_moc_r004.glb");
+  model = await loader.loadAsync(
+    "/models/kyouko_kirigiri_figure_from_danganronpa.glb"
+  );
 }
 
 function setupScene() {
@@ -85,6 +81,7 @@ function setupScene() {
 
 function setupThreeJS() {
   scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xb30303);
 
   // Renderer
   renderer = new THREE.WebGLRenderer({
@@ -109,15 +106,15 @@ function setupThreeJS() {
   // canvas.appendChild(labelRenderer.domElement);
 
   // Lights
-  scene.add(new THREE.AmbientLight(0x404040));
+  scene.add(new THREE.AmbientLight(0xffffff, 1));
 
   //   const lensflare = new Lensflare();
   //   lensflare.addElement(new LensflareElement(textureFlare0, 1000, 0));
   //   light.add(lensflare);
 
   // TODO: Possibly create a point light for the sun
-  //const light = new THREE.DirectionalLight(0xffffff, 0.5);
-  //scene.add(light);
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  scene.add(light);
 
   // // GUI
   // this.gui = new dat.GUI( { autoPlace: false } );
@@ -127,127 +124,12 @@ function setupThreeJS() {
 function setupModel() {
   if (!model) return;
 
-  model.scene.scale.set(0.04, 0.04, 0.04); // scales to ~2 meters
-  model.scene.rotation.set(0, 0, 0);
+  model.scene.scale.set(3, 3, 3);
+  model.scene.rotation.set(0, 2.5, 0);
+  model.scene.position.set(0, -20, 0);
 
   scene.add(model.scene);
 }
-
-//   three.spaceman = models.spaceman.scene;
-//   three.spaceman.scale.set(0.04, 0.04, 0.04); // scales to ~2 meters
-//   three.spaceman.rotation.set(0, 0, 0);
-
-//   // Build shape
-//   if (formData.value.type.shape === "can") {
-//     spacemanPosition += 0.5;
-//     three.spaceman.rotation.x = Math.PI / 2;
-//     three.spaceman.rotation.y = 1.8;
-//     three.spaceman.position.set(1, spacemanPosition, 0); // 6, if solid shape set Z to height of the extrusion
-
-//     if (forces.vectorMesh) {
-//       forces.vectorMesh.position.set(
-//         three.spaceman.position.x - 1.5,
-//         three.spaceman.position.y - 1.5,
-//         three.spaceman.position.z
-//       );
-//     }
-//   } else if (formData.value.type.shape === "cylinder") {
-//     three.spaceman.rotation.y = -Math.PI / 2;
-//     three.spaceman.position.set(
-//       0,
-//       spacemanPosition,
-//       formData.value.shipLength / 2
-//     );
-
-//     // If the radius of the structure is too big our spaceman becomes tiny! let's highlight him with a ring.
-//     if (formData.value.radius > 30) {
-//       const ringGeometry = new THREE.RingGeometry(
-//         3,
-//         formData.value.radius / 20,
-//         32
-//       );
-//       const ringMaterial = new THREE.MeshBasicMaterial({
-//         color: 0xba3700,
-//         side: THREE.DoubleSide,
-//       });
-//       const ringMesh = new THREE.Mesh(ringGeometry, ringMaterial);
-
-//       ringMesh.position.set(
-//         0,
-//         spacemanPosition + 2,
-//         formData.value.shipLength / 2
-//       );
-
-//       three.group.add(ringMesh);
-//     }
-//   } else if (formData.value.type.shape === "bola") {
-//     three.spaceman.rotation.y = -Math.PI / 2;
-//     three.spaceman.position.set(0, spacemanPosition, 0);
-
-//     // If the radius of the structure is too big our spaceman becomes tiny! let's highlight him with a ring.
-//     if (stationWidth.value > 30) {
-//       const ringGeometry = new THREE.RingGeometry(
-//         3,
-//         Math.max(formData.value.shipWidth - 2, 4),
-//         32
-//       );
-//       const ringMaterial = new THREE.MeshBasicMaterial({
-//         color: 0xba3700,
-//         side: THREE.DoubleSide,
-//       });
-//       const ringMesh = new THREE.Mesh(ringGeometry, ringMaterial);
-
-//       ringMesh.position.set(0, spacemanPosition + 2, 0);
-
-//       three.group.add(ringMesh);
-//     }
-//   } else if (formData.value.type.shape === "funnel") {
-//     spacemanPosition += 1;
-
-//     three.spaceman.rotation.z = forces.vector; //Math.PI / 2;
-//     //three.spaceman.rotation.y = 1.8;
-//     three.spaceman.position.set(spacemanPosition, 0, 0); // 6, if solid shape set Z to height of the extrusion
-
-//     //three.group.rotation.x = -Math.PI / 2.5;
-
-//     // If the radius of the structure is too big our spaceman becomes tiny! let's highlight him with a ring.
-//     if (formData.value.radius > 30) {
-//       const ringGeometry = new THREE.RingGeometry(
-//         formData.value.shipLength,
-//         formData.value.shipLength * 1.3,
-//         32
-//       );
-//       const ringMaterial = new THREE.MeshBasicMaterial({
-//         color: 0xba3700,
-//         side: THREE.DoubleSide,
-//       });
-//       const ringMesh = new THREE.Mesh(ringGeometry, ringMaterial);
-
-//       ringMesh.rotation.z = forces.vector;
-
-//       ringMesh.position.set(spacemanPosition + 2, 0, 0);
-
-//       three.group.add(ringMesh);
-//     }
-
-//     if (forces.vectorMesh) {
-//       forces.vectorMesh.position.set(three.spaceman.position.x, -2, 0);
-//     }
-//   }
-//   // // Gui folder
-//   // const spacemanFolder = three.gui.addFolder('Spaceman');
-//   // spacemanFolder.add(three.spaceman.position, 'x', 0, stationWidth.value).name("px");
-//   // spacemanFolder.add(three.spaceman.position, 'y', -stationWidth.value * 2, stationWidth.value * 2).name("py");
-//   // spacemanFolder.add(three.spaceman.position, 'z', 0, stationWidth.value).name("pz");
-
-//   // spacemanFolder.add(three.spaceman.rotation, 'x', -Math.PI / 2, 0).name("rx");
-//   // spacemanFolder.add(three.spaceman.rotation, 'y', -Math.PI / 2, 0).name("ry");
-//   // spacemanFolder.add(three.spaceman.rotation, 'z', -Math.PI / 2, 0).name("rz");
-//   // spacemanFolder.open();
-
-//   three.group.add(three.spaceman);
-
-// }
 
 function onWindowResize() {
   if (!camera) return;
@@ -255,7 +137,11 @@ function onWindowResize() {
   if (!renderer) return;
 
   const width = canvas.getBoundingClientRect().width;
-  const height = width * 0.67;
+  let height = canvas.getBoundingClientRect().height;
+
+  if (height < 300) {
+    height = 300;
+  }
 
   // Update aspect ratio
   camera.aspect = width / height;
@@ -271,9 +157,9 @@ function setupCamera() {
   if (!renderer) return;
 
   // Camera
-  const standardDistance = 1000;
-  const cameraPositionDistance = standardDistance * 2.5;
-  const cameraZoomDistance = standardDistance * 10;
+  const standardDistance = 75;
+  const cameraPositionDistance = standardDistance;
+  const cameraZoomDistance = standardDistance * 6;
 
   let rendererSize = new THREE.Vector2();
   renderer.getSize(rendererSize);
@@ -285,7 +171,7 @@ function setupCamera() {
   );
 
   camera.position.z = cameraPositionDistance;
-  camera.position.y -= cameraPositionDistance;
+
   camera.lookAt(0, 0, 0);
 
   // controls.enableZoom = false;
@@ -295,7 +181,7 @@ function setupCamera() {
   // controls.enableDamping = true;
   // thr.controls.dampingFactor = 0.05;
   controls.maxDistance = cameraZoomDistance;
-  controls.minDistance = 100;
+  controls.minDistance = standardDistance / 4;
 }
 
 function animate() {
