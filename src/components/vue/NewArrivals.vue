6ac944d872d4661cc1aa9e4dbae7200a3f49cae4
jboullion <jboullion83@gmail.com>
2024-05-16 09:24:26 -0500

    change scrolling image dimensions

--- src/components/vue/NewArrivals.vue
+++ src/components/vue/NewArrivals.vue
@@ -18,51 +18,51 @@ import HomeCarousel from "./HomeCarousel.vue";
 
 const images = ref([
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=New+Arrival+1",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=New+Arrival+1",
     altText: "Image 1",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=New+Arrival+2",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=New+Arrival+2",
     altText: "Image 2",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=New+Arrival+3",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=New+Arrival+3",
     altText: "Image 3",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=New+Arrival+4",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=New+Arrival+4",
     altText: "Image 4",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=New+Arrival+5",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=New+Arrival+5",
     altText: "Image 5",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=New+Arrival+6",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=New+Arrival+6",
     altText: "Image 1",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=New+Arrival+7",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=New+Arrival+7",
     altText: "Image 2",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=New+Arrival+8",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=New+Arrival+8",
     altText: "Image 3",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=New+Arrival+9",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=New+Arrival+9",
     altText: "Image 4",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=New+Arrival+10",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=New+Arrival+10",
     altText: "Image 5",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=New+Arrival+11",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=New+Arrival+11",
     altText: "Image 5",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=New+Arrival+12",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=New+Arrival+12",
     altText: "Image 5",
   },
 ]);
--- src/components/vue/PreOrders.vue
+++ src/components/vue/PreOrders.vue
@@ -29,51 +29,51 @@ import HomeCarousel from "./HomeCarousel.vue";
 
 const images = ref([
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=Pre+Order+1",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=Pre+Order+1",
     altText: "Image 1",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=Pre+Order+2",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=Pre+Order+2",
     altText: "Image 2",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=Pre+Order+3",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=Pre+Order+3",
     altText: "Image 3",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=Pre+Order+4",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=Pre+Order+4",
     altText: "Image 4",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=Pre+Order+5",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=Pre+Order+5",
     altText: "Image 5",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=Pre+Order+6",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=Pre+Order+6",
     altText: "Image 1",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=Pre+Order+7",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=Pre+Order+7",
     altText: "Image 2",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=Pre+Order+8",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=Pre+Order+8",
     altText: "Image 3",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=Pre+Order+9",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=Pre+Order+9",
     altText: "Image 4",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=Pre+Order+10",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=Pre+Order+10",
     altText: "Image 5",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=Pre+Order+11",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=Pre+Order+11",
     altText: "Image 5",
   },
   {
-    imageSrc: "https://placehold.co/300x200/000000/F00?text=Pre+Order+12",
+    imageSrc: "https://placehold.co/300x400/000000/F00?text=Pre+Order+12",
     altText: "Image 5",
   },
 ]);
